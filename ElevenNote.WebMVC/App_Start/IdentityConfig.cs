9ba4061f3c0ac597bfe30124345d0eb8114ccd61
Andrea-Kimball <aundreya.7@gmail.com>
2020-03-25 09:46:37 -0400

    Index view

--- ElevenNote.WebMVC/App_Start/IdentityConfig.cs
+++ ElevenNote.WebMVC/App_Start/IdentityConfig.cs
@@ -55,10 +55,10 @@ namespace ElevenNote.WebMVC
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
                 RequireLowercase = true,
-                RequireUppercase = true,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
